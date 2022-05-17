e10165db12fd714de80bde9faf5488e742d63cca
Russell Purinton <russell.purinton@icloud.com>
2022-05-17 20:16:29 +0000

    fix emoji in names

--- modules/core-functions.php
+++ modules/core-functions.php
@@ -2,8 +2,16 @@
 
 function firstname($name)
 {
+	$r = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
 	$name2 = explode(" ", $name);
-	return $name2[0];
+	$first = $name2[0];
+	$cleanfirst = "";
+	for($i = 0; $i < strlen($first); $i++)
+	{
+		$char = strtolower(substr($first,$i,1));
+		if(array_search($char,$r) !== false) $cleanfirst .= substr($first,$i,1);
+	}
+	return $cleanfirst;
 }
 
 function my_replace($search, $replace, $mixed)
